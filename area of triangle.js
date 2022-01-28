e92b74b445361e424a39ab41a92fe75eaf6cbee4
yubi012 <98576583+yubi012@users.noreply.github.com>
2022-01-28 12:59:59 +0530

    Add files via upload

---
+++ area of triangle.js
@@ -0,0 +1,8 @@
+const base = prompt('Enter the base of a triangle: ');
+const height = prompt('Enter the height of a triangle: ');
+
+const area = (base * height) / 2;
+
+console.log(
+  'The area of the triangle is ${area}'
+);
